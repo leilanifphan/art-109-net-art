15454c5c6d2c804e415d6570d61369db41aac7e0
leilanifphan <leilanifphan@gmail.com>
2025-10-02 15:43:13 -0700

    added instructions popup

--- js/main.js
+++ js/main.js
@@ -17,12 +17,30 @@ document.querySelector('#smiski-book').addEventListener('click', function() {
 // toggle smiski popup
 const smiskiImg = document.querySelector('#smiski-book');
 const smiskiWrapper = document.querySelector('.smiski-book');
-const smiskiClose = document.querySelector('.smiski-book-text .close-btn');
+const smiskiCloseBook = document.querySelector('.smiski-book-text .close-btn');
 
 smiskiImg?.addEventListener('click', () => {
 	smiskiWrapper?.classList.toggle('open');
 });
 
-smiskiClose?.addEventListener('click', () => {
+smiskiCloseBook?.addEventListener('click', () => {
 	smiskiWrapper?.classList.remove('open');
+});
+
+
+// toggle instructions popup
+const instructionsBtn = document.querySelector('.instructions-btn');
+const instructionsWrapper = document.querySelector('.instructions');
+const instructionsCloseBtn = document.querySelector('.instructions .close-btn');
+
+instructionsBtn?.addEventListener('click', () => {
+	if (instructionsWrapper) {
+		instructionsWrapper.style.display = 'block';
+	}
+});
+
+instructionsCloseBtn?.addEventListener('click', () => {
+	if (instructionsWrapper) {
+		instructionsWrapper.style.display = 'none';
+	}
 });
